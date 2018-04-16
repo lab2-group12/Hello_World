4c75aa1858a915092239292223b76793acfb0f5a
VitaminLLL <38287479+VitaminLLL@users.noreply.github.com>
2018-04-16 17:17:33 +0200

    second

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("i am li xiao ye!");
+        System.out.println("Ssssss");
     }
 }
